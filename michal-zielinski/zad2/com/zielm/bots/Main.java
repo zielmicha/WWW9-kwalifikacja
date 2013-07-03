04ef602c852e14619f445eb32bf6209669f3cd32
Michał Zieliński <michal@zielinscy.org.pl>
2013-07-03 12:46:33 +0200

    actually perform actions, UI

--- michal-zielinski/zad2/com/zielm/bots/Main.java
+++ michal-zielinski/zad2/com/zielm/bots/Main.java
@@ -4,15 +4,36 @@ public class Main {
     public static void main(String[] args) {
         World world = new World();
         world.addBot(new RandomAI(), 5, 5);
-        world.addBot(new RandomAI(), 15, 5);
+        world.addBot(new RandomAI(), 1, 0);
         simulate(world);
     }
 
-    public static void simulate(World world) {
+    public static void simulate(final World world) {
         while(world.getNumberOfBotsAlive() > 1) {
-            world.simulate();
-            Utils.sleep(0.1);
+            world.simulate(new Runnable() {
+                    public void run() {
+                        printWorld(world);
+                    }
+                });
+            printWorld(world);
+            Utils.sleep(0.3);
         }
     }
 
+    public static void printWorld(World world) {
+        for(int y=0; y < world.h; y++) {
+            for(int x=0; x < world.w; x++) {
+                char c = 'X';
+                char d = ' ';
+                World.Bot bot = world.getBotAt(x, y);
+                if(bot != null) {
+                    c = (char)((int)'A' + bot.id - 1);
+                    d = bot.getDirectionLetter();
+                }
+                System.out.print(" " + c + "" + d);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
--- michal-zielinski/zad2/com/zielm/bots/World.java
+++ michal-zielinski/zad2/com/zielm/bots/World.java
@@ -6,17 +6,46 @@ import java.util.*;
 public class World {
     private Map<Bot, BotAI> ais = new HashMap<Bot, BotAI>();
     private List<Bot> botOrder = new ArrayList<Bot>();
-    public final int w = 8;
-    public final int h = 8;
-    public final int numberOfSteps = 5;
+    public final static int w = 8;
+    public final static int h = 8;
+    public final static int numberOfSteps = 5;
 
     public static class Bot {
         private int hp = 5;
         private int x, y;
+        private int dir = 0;
+        public final int id;
 
-        public Bot(int x, int y) {
+        public char getDirectionLetter() {
+            switch(dir) {
+            case 0:
+                return 'v';
+            case 1:
+                return '>';
+            case 2:
+                return '^';
+            case 3:
+                return '<';
+            default:
+                return '?';
+            }
+        }
+
+        private void changeDir(int delta) {
+            dir += delta;
+            dir = mod(dir, 4);
+        }
+
+        public Bot(int x, int y, int id) {
             this.x = x;
             this.y = y;
+            this.id = id;
+            normalize();
+        }
+
+        private void normalize() {
+            x = mod(x, w);
+            y = mod(y, h);
         }
 
         public int getHp() {
@@ -26,15 +55,21 @@ public class World {
         public boolean isAlive() {
             return getHp() > 0;
         }
+
+        public String toString() {
+            return String.format("<Bot at %d pos=(%d,%d) hp=%d>",
+                                 System.identityHashCode(this),
+                                 x, y, hp);
+        }
     }
 
     public void addBot(BotAI ai, int x, int y) {
-        Bot bot = new Bot(x, y);
+        Bot bot = new Bot(x, y, botOrder.size() + 1);
         ais.put(bot, ai);
         botOrder.add(bot);
     }
 
-    public void simulate() {
+    public void simulate(Runnable runBetweenSteps) {
         HashMap<Bot, LinkedList<BotAction>> expectedActions = new HashMap<Bot, LinkedList<BotAction>>();
         for(Map.Entry<Bot, BotAI> e: ais.entrySet()) {
             Bot bot = e.getKey();
@@ -46,6 +81,7 @@ public class World {
                 BotAction actionToTake = expectedActions.get(bot).removeFirst();
                 takeAction(bot, actionToTake);
             }
+            runBetweenSteps.run();
         }
     }
 
@@ -57,8 +93,8 @@ public class World {
         return count;
     }
 
-    public void takeAction(Bot bot, BotAction action) {
-        System.err.printf("%s does %s\n", bot, action);
+    private void takeAction(Bot bot, BotAction action) {
+        //System.err.printf("%s does %s\n", bot, action);
         if(!bot.isAlive()) return;
         switch(action) {
         case SHOOT:
@@ -66,13 +102,46 @@ public class World {
         case HIT:
             break;
         case FORWARD:
+            tryMove(bot, 1);
             break;
         case BACKWARD:
+            tryMove(bot, -1);
             break;
         case LEFT:
+            bot.changeDir(1);
             break;
         case RIGHT:
+            bot.changeDir(-1);
             break;
+        }
+    }
+
+    public Bot getBotAt(int x, int y) {
+        x = mod(x, w);
+        y = mod(y, h);
+        for(Bot bot : botOrder) {
+            if(bot.x == x && bot.y == y)
+                return bot;
+        }
+        return null;
+    }
+
+    private void tryMove(Bot bot, int dir) {
+        int botDir = bot.dir;
+        if(botDir == 2 || botDir == 3) {
+            botDir %= 2;
+            dir *= -1;
+        }
+        int x = botDir == 1 ? dir : 0;
+        int y = botDir == 0 ? dir : 0;
+        tryMoveTo(bot, bot.x + x, bot.y + y);
+    }
+
+    private void tryMoveTo(Bot bot, int newX, int newY) {
+        if(getBotAt(newX, newY) == null) {
+            bot.x = newX;
+            bot.y = newY;
+            bot.normalize();
         }
     }
 }
